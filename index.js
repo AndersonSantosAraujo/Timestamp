2899c277b394f78a71bc1b70de066aba9d1048cd
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2023-11-27 21:15:36 -0300

    feat: added rate limit

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import moment from "moment-timezone";
 import express from "express";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 const port = 3000;
 
-app.get("/time.stamp", (req, res) => {
+const loginLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 min
+  max: 3,
+  handler: (req, res) => {
+    return res
+      .status(429)
+      .send("Too many requests. Please try again in 10 minutes.");
+  },
+});
+
+app.get("/time.stamp", loginLimiter, (req, res) => {
   const now = moment.tz("America/Sao_Paulo");
   const timestamp = now.valueOf();
 
